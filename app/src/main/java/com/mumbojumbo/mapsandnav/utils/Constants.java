84df9f0cc71ec35e4794176452431661bb0f6981
Arjan <mundyarjan@gmail.com>
2020-02-21 00:24:02 -0600

    Initial Commit
    1) Maps & Locations API Configured and included in the app.
    2) Maps API to be included in a separated file called api.properties.
    3) Permissions and Services Configuration complete.

---
+++ app/src/main/java/com/mumbojumbo/mapsandnav/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.mumbojumbo.mapsandnav.utils;
+
+public class Constants {
+    public static final int PERMISSIONS_ENABLE_GPS = 7357;
+    public static final int PERMISSIONS_ACCESS_FINE_LOCATION = 7357;
+    public static final int ERROR_DIALOG_MSG = 3751;
+
+}
